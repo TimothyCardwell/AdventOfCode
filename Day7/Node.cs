bf756d7576f0547e11f811a2eacff78bf69d08e1
Tim Cardwell <timothy.cardwell@gmail.com>
2017-12-08 09:36:00 -0600

    Day 7

---
+++ Day7/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public int Weight { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node(string value, int weight, List<Node> children)
+        {
+            Value = value;
+            Weight = weight;
+            Children = children ?? new List<Node>();
+        }
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day7
+{
+    class Program
+    {
+        private const string Input = @"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)";
+        static void Main(string[] args)
+        {
+            Dictionary<string, Tuple<string, int, string[]>> parsedInput = new Dictionary<string, Tuple<string, int, string[]>>();
+            HashSet<string> parentNodes = new HashSet<string>();
+            HashSet<string> childNodes = new HashSet<string>();
+            using(StreamReader streamReader = new StreamReader("input.txt"))
+            //using(StringReader streamReader = new StringReader(Input))
+            {
+                string line;
+                while((line = streamReader.ReadLine()) != null)
+                {
+                    string[] node;
+                    string[] children;
+                    if (line.Contains(" -> "))
+                    {
+                        string[] nodeAndChildren = line.Split(" -> ");
+                        node = nodeAndChildren[0].Split(" ");
+                        children = nodeAndChildren[1].Split(", ");
+
+
+                    }
+                    else
+                    {
+                        node = line.Split(" ");
+                        children = null;
+                    }
+
+                    string value = node[0];
+                    int weight = Convert.ToInt32(node[1].Substring(1, node[1].Length - 2));
+                    parsedInput.Add(value, new Tuple<string, int, string[]>(value, weight, children));
+                    if(children != null)
+                    {
+                        parentNodes.Add(value);
+                        foreach(string child in children)
+                        {
+                            childNodes.Add(child);
+                        }
+                    }
+                }
+            }
+
+            parentNodes.ExceptWith(childNodes);
+            Tuple<string, int, string[]> rootNodeTuple = null;
+            foreach(string key in parsedInput.Keys)
+            {
+                if(parentNodes.Contains(key))
+                {
+                    rootNodeTuple = parsedInput[key];
+                    break;
+                }
+            }
+
+            Node rootNode = BuildTree(rootNodeTuple, parsedInput);
+
+            SumWeights(rootNode);
+
+            Console.WriteLine(rootNode.Value);
+            Console.ReadLine();
+        }
+
+        private static Node BuildTree(Tuple<string, int, string[]> rootNode, Dictionary<string, Tuple<string, int, string[]>> allNodes)
+        {
+            Node node = new Node(rootNode.Item1, rootNode.Item2, null);
+
+            if (rootNode.Item3 != null)
+            {
+                foreach (string childNode in rootNode.Item3)
+                {
+                    node.Children.Add(BuildTree(allNodes[childNode], allNodes));
+                }
+            }
+
+            return node;
+        }
+
+        private static int SumWeights(Node rootNode)
+        {
+            if(rootNode.Children.Count == 0)
+            {
+                return rootNode.Weight;
+            }
+            else
+            {
+                int totalWeight = 0;
+                List<int> uniqueWeights = new List<int>();
+                foreach (Node childNode in rootNode.Children)
+                {
+                    int childWeight = SumWeights(childNode);
+                    totalWeight += childWeight;
+
+                    //if (!uniqueWeights.Contains(childWeight))
+                    //{
+                    //    uniqueWeights.Add(childWeight);
+                    //}
+                }
+
+                //if(uniqueWeights.Count > 1)
+                //{
+                //    Console.WriteLine(Math.Abs(uniqueWeights[0] - uniqueWeights[1]));
+                //    Console.ReadLine();
+                //}
+                return totalWeight + rootNode.Weight;
+            }
+        }
+    }
+}
