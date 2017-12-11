68cb135a9636e86e2314a34763086d13a021133f
Tim Cardwell <Timothy.cardwell@gmail.com>
2017-12-10 18:23:41 -0600

    Day10 Part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -6,50 +6,119 @@ namespace Day10
 {
     class Program
     {
-        private static int[] Knots = Enumerable.Range(0, 256).ToArray();
-        private static int[] Groupings = new int[] { 230, 1, 2, 221, 97, 252, 168, 169, 57, 99, 0, 254, 181, 255, 235, 167 };
+        private const string Input = "230, 1, 2, 221, 97, 252, 168, 169, 57, 99, 0, 254, 181, 255, 235, 167";
         static void Main(string[] args)
         {
-            int currentIndex = 0;
-            int skipCount = 0;
-            for(int i = 0; i < Groupings.Length; i++)
-            {
-                int currentGroupSize = Groupings[i];
+            int[] sparseHash = Enumerable.Range(0, 256).ToArray();
 
-                for(int j = 0; j < currentGroupSize / 2; j++)
+            #region Part1
+            // Part 1
+            /*
+            int[] lengths = new int[] { 230, 1, 2, 221, 97, 252, 168, 169, 57, 99, 0, 254, 181, 255, 235, 167 };
+            int[] input = Input.Replace(" ", "").Split(",").Select(x => Convert.ToInt32(x)).ToArray();
+            Knot(sparseHash, input, 0, 0);
+            Console.WriteLine(input[0] * input[1]);
+            Console.ReadLine();
+            */
+            #endregion
+
+            #region Part2
+            // Part 2
+            string inputString = Input.Replace(" ", ""); // Remove white space
+
+            // Convert input to ASCII
+            int[] inputAscii = new int[inputString.Length + 5];
+            for(int i = 0; i < inputString.Length; i++)
+            {
+                inputAscii[i] = inputString[i];
+            }
+            inputAscii[inputString.Length] = 17;
+            inputAscii[inputString.Length + 1] = 31;
+            inputAscii[inputString.Length + 2] = 73;
+            inputAscii[inputString.Length + 3] = 47;
+            inputAscii[inputString.Length + 4] = 23;
+
+            // Knot the input 64 times
+            int skipCount = 0;
+            int index = 0;
+            for (int i = 0; i < 64; i++)
+            {
+                Tuple<int, int> response = Knot(sparseHash, inputAscii, index, skipCount);
+                index = response.Item1;
+                skipCount = response.Item2;
+            }
+
+            // Convert to dense hash
+            int[] denseHash = new int[16];
+            for(int i = 0; i < 16; i++)
+            {
+                denseHash[i] = 
+                    sparseHash[(i * 16) + 0] ^ 
+                    sparseHash[(i * 16) + 1] ^
+                    sparseHash[(i * 16) + 2] ^
+                    sparseHash[(i * 16) + 3] ^
+                    sparseHash[(i * 16) + 4] ^
+                    sparseHash[(i * 16) + 5] ^
+                    sparseHash[(i * 16) + 6] ^
+                    sparseHash[(i * 16) + 7] ^
+                    sparseHash[(i * 16) + 8] ^
+                    sparseHash[(i * 16) + 9] ^
+                    sparseHash[(i * 16) + 10] ^
+                    sparseHash[(i * 16) + 11] ^
+                    sparseHash[(i * 16) + 12] ^
+                    sparseHash[(i * 16) + 13] ^
+                    sparseHash[(i * 16) + 14] ^
+                    sparseHash[(i * 16) + 15];
+            }
+
+            // Translate to hex values
+            string[] denseHashHex = denseHash.Select(x => {
+                string hexValue = x.ToString("X");
+                return hexValue.Length == 1 ? "0" + hexValue : hexValue;
+                }).ToArray();
+            #endregion
+
+            Console.WriteLine(String.Join(String.Empty, denseHashHex));
+            Console.ReadLine();
+    }
+
+        private static Tuple<int, int> Knot(int[] source, int[] lengths, int index, int skipCount)
+        {
+            for (int i = 0; i < lengths.Length; i++)
+            {
+                int currentGroupSize = lengths[i];
+
+                for (int j = 0; j < currentGroupSize / 2; j++)
                 {
                     // Determine left index
-                    int leftSwapIndex = currentIndex + j;
-                    if(leftSwapIndex >= Knots.Length)
+                    int leftSwapIndex = index + j;
+                    if (leftSwapIndex >= source.Length)
                     {
-                        leftSwapIndex = leftSwapIndex - Knots.Length;
+                        leftSwapIndex = leftSwapIndex - source.Length;
                     }
 
                     // Determine right index
-                    int rightSwapIndex = currentIndex + currentGroupSize - j - 1;
-                    if (rightSwapIndex >= Knots.Length)
+                    int rightSwapIndex = index + currentGroupSize - j - 1;
+                    if (rightSwapIndex >= source.Length)
                     {
-                        rightSwapIndex = rightSwapIndex - Knots.Length;
+                        rightSwapIndex = rightSwapIndex - source.Length;
                     }
 
                     // Swap values
-                    int temp = Knots[leftSwapIndex];
-                    Knots[leftSwapIndex] = Knots[rightSwapIndex];
-                    Knots[rightSwapIndex] = temp;
+                    int temp = source[leftSwapIndex];
+                    source[leftSwapIndex] = source[rightSwapIndex];
+                    source[rightSwapIndex] = temp;
                 }
 
-                //Console.WriteLine(String.Join(", ", Knots));
-                
-                currentIndex = currentIndex + currentGroupSize + skipCount;
-                if(currentIndex >= Knots.Length)
+                index = index + currentGroupSize + skipCount;
+                if (index >= source.Length)
                 {
-                    currentIndex = currentIndex % Knots.Length;
+                    index = index % source.Length;
                 }
                 skipCount++;
             }
 
-            Console.WriteLine(Knots[0] * Knots[1]);
-            Console.ReadLine();
+            return new Tuple<int, int>(index, skipCount);
         }
     }
 }
