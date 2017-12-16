2f00396569f7efc45f5b4bfe8a06336cc439b366
Tim Cardwell <Timothy.cardwell@gmail.com>
2017-12-16 14:50:47 -0600

    Day 16

---
+++ Day16/DanceMove.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day16
+{
+    public abstract class DanceMove
+    {
+        public abstract string DanceMoveKey { get; set; }
+        public abstract void PerformMove(ref string[] currentPositions);
+    }
+}
